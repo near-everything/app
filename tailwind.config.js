629529655f86249e3a2e7872a313d35c44aca2f0
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2022-08-23 15:31:03 -0400

    styles: WIP daisy-ui conversion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,22 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  darkMode: 'class',
-  theme: {
-    extend: {},
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  plugins: [require("@tailwindcss/forms"), require("daisyui")],
+  daisyui: {
+    themes: ["forest",
+      {
+        main: {
+          primary: "#2d56a3",
+          secondary: "#f7b9ee",
+          accent: "#9767db",
+          neutral: "#141B24",
+          "base-100": "#303540",
+          info: "#7797F8",
+          success: "#1D7C5B",
+          warning: "#EFA14D",
+          error: "#F06C60",
+        },
+      },
+    ],
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require("daisyui")
-  ],
-}
+};
