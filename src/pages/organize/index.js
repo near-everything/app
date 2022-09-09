bc89740cbb8324923274ffda45a622b14aea02b6
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2022-09-09 14:40:18 -0400

    fix: build issues

--- src/pages/organize/index.js
+++ src/pages/organize/index.js
@@ -1,38 +1,10 @@
-import { parseCookies } from "nookies";
-import React, { useState } from "react";
-import getFirebaseAdmin from "../../app/firebaseAdmin";
-import InfiniteRequests from "../../components/Organize/InfiniteRequests";
-import InfiniteThings from "../../components/Organize/InfiniteThings";
+import React from "react";
 import Layout from "../../containers/Layout";
 
-export async function getServerSideProps(ctx) {
-  try {
-    const admin = getFirebaseAdmin();
-    const cookies = parseCookies(ctx);
-    await admin.auth().verifyIdToken(cookies.__session);
-
-    return {
-      props: {},
-    };
-  } catch (err) {
-    // either the `__session` cookie didn't exist
-    // or token verification failed
-    // either way: redirect to the login page
-    return {
-      redirect: {
-        permanent: false,
-        destination: "/login",
-      },
-      props: {},
-    };
-  }
-}
-
 function Organize() {
-  const [list, setList] = useState("things");
   return (
     <>
-      <div className="flex flex-col h-full px-6 py-6">
+      {/* <div className="flex flex-col h-full px-6 py-6">
         <div className="flex flex-row w-full justify-between">
           <div className="flex flex-col w-full">
             <div className="flex w-full justify-between">
@@ -78,10 +50,10 @@ function Organize() {
             Toggle
           </button>
           <div id="list-container">
-            {list === "things" ? <InfiniteThings /> : <InfiniteRequests />}
+            <InfiniteThings />
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
