6e065888e7f6a8567c5b035e33f70c0e7cc23ba4
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2022-11-03 13:31:49 -0400

    fixes gitignore

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,21 @@ import Layout from "../containers/Layout";
 function Home() {
   return (
     <>
-      <Header title="everything" />
+      <Header title="everything | feed" />
+      <div className="flex h-full w-full justify-center items-center">
+        <div className="text-center">
+          <p>this is a work in progress</p>
+          <p>have feedback or want to help?</p>
+          <a
+            href="https://about.everything.dev/contact"
+            target={"_blank"}
+            rel="noreferrer"
+            className="text-blue-600 hover:text-blue-800"
+          >
+            send us a message
+          </a>
+        </div>
+      </div>
     </>
   );
 }
