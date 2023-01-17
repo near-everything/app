1d1d6a3be6443ef940bf1bdca7986469f055a811
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2023-01-16 23:14:28 -0500

    initial commit

--- app/api.js
+++
@@ -1,5 +0,0 @@
-import { GraphQLClient } from "graphql-request";
-
-export const BUCKET_URL = "https://everything-1.s3.us-east-1.amazonaws.com/";
-
-export const graphqlClient = new GraphQLClient("/api/graphql");
