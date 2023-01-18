5dd5fe18d45afdb0ee9da1853d47015967d2ee04
gurambalavadze <gurambalavadze@gmail.com>
2023-01-18 21:16:12 +0330

    add fonts

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import "@fontsource/inter";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
