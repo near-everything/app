4f6a2bec16547168c055b8ef445eadc38f7fc62f
gurambalavadze <gurambalavadze@gmail.com>
2023-01-18 20:49:58 +0330

    feat starter documents

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
