3519cd960254c46c5ee1cd4daacbe830da2f52e4
gurambalavadze <gurambalavadze@gmail.com>
2023-02-06 19:39:31 +0330

    fix take photo

--- src/component/create/index.tsx
+++ src/component/create/index.tsx
@@ -3,40 +3,36 @@ import { ReactComponent as Arrow } from "assets/icon/close.svg";
 import { ReactComponent as Gallery } from "assets/icon/gallery.svg";
 import { Secondarywhite } from "component/shared/btn";
 import { MediaOnboardingDialog } from "component/shared/mediaonboardingdialog";
+import Webcam from "react-webcam";
 
 type Props = {};
-
+const FACING_MODE_USER = "user";
+const FACING_MODE_ENVIRONMENT = "environment";
 function Index({}: Props) {
-  const videoRef = useRef(null);
-  const photoRef = useRef(null);
-  useEffect(() => {
-    getVideo();
-  }, [videoRef]);
-  const getVideo = async () => {
-    await navigator.mediaDevices
-      .getUserMedia({
-        video: {
-          width: 1920,
-          height: 1080,
-        },
-      })
-      .then((stream) => {
-        let video = document.createElement("video");
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+  const webcamRef = useRef<Webcam>(null);
+  const [url, setUrl] = useState<string | null>(null);
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    if (imageSrc) {
+      setUrl(imageSrc);
+    }
+  }, [webcamRef]);
+  const [facingMode, setFacingMode] = React.useState(FACING_MODE_USER);
+
+  let videoConstraints: MediaTrackConstraints = {
+    facingMode: facingMode,
+    width: 270,
+    height: 480,
   };
-  const takePhoto = () => {
-    const width = 414;
-    const height = width / (16 / 9);
-    let video = videoRef.current;
-    let photo = photoRef.current;
-    // photo.width = width;
-    // photo.height = height;
-  };
+
+  const handleClick = React.useCallback(() => {
+    setFacingMode((prevState) =>
+      prevState === FACING_MODE_USER
+        ? FACING_MODE_ENVIRONMENT
+        : FACING_MODE_USER
+    );
+  }, []);
+
   return (
     <div className=" bg-black  h-full">
       <MediaOnboardingDialog />
@@ -46,14 +42,28 @@ function Index({}: Props) {
         </div>
       </div>
 
-      <div className=" mt-[16px] bg-white20  w-[97%] h-[60%]  rounded-[24px] mx-auto">
-        <Mirror></Mirror>
+      <div className=" mt-[16px] bg-white20  w-[97%] h-[60%]  rounded-[24px] mx-auto overflow-hidden">
+        {url ? (
+          <img src={url} alt="Screenshot" />
+        ) : (
+          <Webcam
+            audio={false}
+            width={"100%"}
+            height={"100%"}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            videoConstraints={videoConstraints}
+          />
+        )}
       </div>
       <div className=" absolute bottom-[30px] left-0 right-[-16px]  flex items-center justify-between w-[85%] mx-auto">
         <div className=" p-[15px] rounded-[50%] bg-white20 text-white flex items-center justify-center cursor-pointer">
           <Gallery />
         </div>
-        <div className=" w-[64px] h-[64px] rounded-[50%] bg-white cursor-pointer"></div>
+        <div
+          className=" w-[64px] h-[64px] rounded-[50%] bg-white cursor-pointer"
+          onClick={capture}
+        />
         <Secondarywhite
           size="L"
           type="button"
@@ -68,21 +78,3 @@ function Index({}: Props) {
 }
 
 export default Index;
-interface State {}
-class Mirror extends React.Component<Props, State> {
-  video_reference: HTMLVideoElement | null = null;
-  set_video_reference = (element: HTMLVideoElement) => {
-    this.video_reference = element;
-  };
-  async componentDidMount() {
-    if (this.video_reference) {
-      let video_stream: MediaStream = await navigator.mediaDevices.getUserMedia(
-        { video: { facingMode: "user" }, audio: false }
-      );
-      this.video_reference.srcObject = video_stream;
-    }
-  }
-  render() {
-    return <video ref={this.set_video_reference} id="player" autoPlay></video>;
-  }
-}
