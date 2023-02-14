975c9005d56a23c31be1afccf9b5c8809a1b62f7
gurambalavadze <gurambalavadze@gmail.com>
2023-02-14 04:28:14 +0330

    fix template

--- src/component/template/applytemplate.tsx
+++ src/component/template/applytemplate.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ReactComponent as Arrow } from "assets/icon/close.svg";
 import { ReactComponent as Add } from "assets/icon/add.svg";
+import { ReactComponent as Shirt } from "assets/icon/templates/shirt.svg";
 
 type Props = {
   setClose: (close: boolean) => void;
@@ -33,9 +34,72 @@ function Applytemplate({ setClose }: Props) {
         </div>
         <p className=" text-Button16 ml-[8px]">new template</p>
       </div>
-      <p className=" mt-[24px] text-Body16">our collection</p>
+      <p className=" mt-[24px] text-Body16 mb-[16px]">our collection</p>
+      <div>
+        {temps.map((item, id) => (
+          <div key={id} className="flex items-center justify-start mb-[8px]">
+            <div className=" bg-gray-90 rounded-[18px] w-[48px] h-[48px] flex items-center justify-center">
+              {item?.ico}
+            </div>
+            <div className=" ml-[10px] flex flex-col items-start justify-start">
+              <p className="text-Button16 mb-[2px]">{item.title}</p>
+              <p className="text-caption12 mb-[2px] text-gray-30">
+                {item.description}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default Applytemplate;
+
+const temps = [
+  {
+    ico: <Shirt />,
+    title: "shirt",
+    description: "color, brand, size, price, material",
+  },
+  {
+    ico: <Shirt />,
+    title: "pants",
+    description: "color, brand, size, price, material",
+  },
+  {
+    ico: <Shirt />,
+    title: "dress",
+    description: "color, brand, size, price, material",
+  },
+  {
+    ico: <Shirt />,
+    title: "shoes",
+    description: "color, brand, size, price, material",
+  },
+  {
+    ico: <Shirt />,
+    title: "glasses",
+    description: "color, brand, size, price, material",
+  },
+  {
+    ico: <Shirt />,
+    title: "jewellery",
+    description: "color, brand, size, price, material",
+  },
+  {
+    ico: <Shirt />,
+    title: "bag",
+    description: "color, brand, size, price, material",
+  },
+  {
+    ico: <Shirt />,
+    title: "book",
+    description: "color, brand, size, price, material",
+  },
+  {
+    ico: <Shirt />,
+    title: "videogame",
+    description: "color, brand, size, price, material",
+  },
+];
