e07887a26573941ead9c86e1206bdede99d5b78b
gurambalavadze <gurambalavadze@gmail.com>
2023-02-21 20:20:22 +0330

    fix profile

---
+++ src/component/profile/footer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { ReactComponent as Circle } from "assets/icon/profile/circle.svg";
+import { ReactComponent as Add } from "assets/icon/add.svg";
+
+type Props = {};
+
+const Footer = (props: Props) => {
+  return (
+    <div className="px-[54px] py-[24px] absolute bottom-[16px] right-[8px] left-[8px] bg-black80 backdrop-blur-[12px] rounded-[24px] flex items-center justify-between">
+      <div>
+        <Circle />
+      </div>
+      <div>
+        <Add className=" w-[32px] h-[32px] text-white" />
+      </div>
+      <div className=" w-[24px] h-[24px] bg-white rounded-[50%] cursor-pointer" />
+    </div>
+  );
+};
+
+export default Footer;
--- src/component/profile/index.tsx
+++ src/component/profile/index.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
-
-type Props = {}
+import React, { useState } from "react";
+import { ReactComponent as User } from "assets/icon/profile/user.svg";
+import { ReactComponent as Setting } from "assets/icon/profile/setting.svg";
+import Footer from "./footer";
+type Props = {};
 
 const Index = (props: Props) => {
+  const [data, setData] = useState([]);
   return (
-    <div>index</div>
-  )
-}
+    <div className=" w-full h-full relative">
+      <div className=" flex items-center justify-between px-[16px] py-[4px]">
+        <div className=" bg-gray-10 p-[14px] rounded-[50%]">
+          <User />
+        </div>
+        <div className=" border-gray-20 border-solid w-[48px] h-[48px] border rounded-[50%]"></div>
+        <div className=" bg-gray-10 p-[14px] rounded-[50%]">
+          <Setting />
+        </div>
+      </div>
+      <div className=" mt-[16px] flex items-center justify-center ">
+        <p className=" text-black text-title24">my everything</p>
+      </div>
+      <div className="mt-[40px] mb-[12px] grid grid-cols-2 text-center w-[85%] mx-auto">
+        <p className="text-black text-Body16">collections</p>
+        <p className="text-black text-Body16">things</p>
+      </div>
+      <div className=" bg-black rounded-t-[22px] w-full h-full relative">
+        {data.length === 0 ? (
+          <div className=" bg-gray-95 rounded-[22px] h-[55%] relative mx-[4px] mt-[4px]">
+            <div className=" absolute top-[50%] w-full left-[50%] -translate-x-center -translate-y-center ">
+              <div className=" flex flex-col items-center justify-center">
+                <p className=" text-gray-40 text-Body14">nothing here yet :(</p>
+                <p className=" text-title24">let’s add your first thing</p>
+                <pre className=" mt-[24px] text-gray-40 text-Body14 text-center">
+                  {"click the plus button on the" + "\n" + "navbar to start"}
+                </pre>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-export default Index
+export default Index;
