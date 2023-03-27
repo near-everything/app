872ce5c0193372a1e266811a1602627ed25df8a3
gurambalavadze <gurambalavadze@gmail.com>
2023-03-27 05:05:27 +0430

    fix setting with no account

--- src/component/setting/index.tsx
+++ src/component/setting/index.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as Arrow } from "assets/icon/arrowleft.svg";
-
+import Footer from "component/profile/footer";
+import Noaccount from "./noaccount";
+import Hasaccount from "./hasaccount";
 type Props = {};
 
 const Index = (props: Props) => {
+  const [account, setAccount] = useState(false);
   return (
     <div className=" h-full w-full">
       <div className=" flex items-center justify-between mt-[4px] px-[16px] mb-[12px] bg-white w-full">
@@ -13,9 +16,14 @@ const Index = (props: Props) => {
         <div>
           <p className=" text-Body16 text-black ">settings</p>{" "}
         </div>
-        <div></div>
+        <div className=" bg-gray-10 rounded-[50%] p-[14px] opacity-0">
+          <Arrow className=" text-black" />
+        </div>
       </div>
-      <div className=" bg-black rounded-t-[22px] h-full"></div>
+      <div className=" bg-black rounded-t-[22px] h-full p-[4px]">
+        {!account ? <Noaccount /> : <Hasaccount />}
+      </div>
+      <Footer />
     </div>
   );
 };
